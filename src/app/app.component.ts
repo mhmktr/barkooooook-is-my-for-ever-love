caf9a4ae3846a0bdcebd95fc8944a1b0890679c8
m.mokhtari <mhmokhtari3@gmail.com>
2022-03-16 16:43:33 +0330

    ali barkook ali barkook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  age:number=55;
+  name:string="jigar tala"
   title = 'my-app';
+  lastName: any;
+ 
+  allow: boolean = false;
+  constructor(){
+setTimeout(()=>{
+  this.allow=true;
+},5000);
+
+  }
+  getName(){
+    return this.name;
+  }
+
+  // onInputChange(event:Event){
+  //   this.lastName=(<HTMLInputElement>event.target).value;
+  // }
+  onSubmit(){
+    console.log(this.lastName);
+    
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
